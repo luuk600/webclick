f0bf824578eeab7d0a5f40430860ac1faf64984f
luuk600 <119942867+luuk600@users.noreply.github.com>
2024-02-08 11:00:06 +0100

    some javascript
    javascript trying something to find the target id

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,11 @@
 //document.getElementById("mainTitle").innerText = "hello world";
+
+// gameWindow reference 
+const gameWindow = document.getElementById("gameWindow");
+// character reference
+const mainCharacter = document.getElementById("mainCharacter")
+
+
+gameWindow.onclick = function (e) {
+    console.log(e.target.id);
+}
