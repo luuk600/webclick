2bb90f3a5d3e61c786b906698f3803a06c464f93
luuk600 <119942867+luuk600@users.noreply.github.com>
2024-02-08 12:54:35 +0100

    adding img
    added the image and door and continued with javascript

--- javascript/main.js
+++ javascript/main.js
@@ -5,7 +5,7 @@ const gameWindow = document.getElementById("gameWindow");
 // character reference
 const mainCharacter = document.getElementById("mainCharacter")
 const ofsetCharacter = 16;
-
+const sign = document.getElementById("sign");
 gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
@@ -14,4 +14,24 @@ gameWindow.onclick = function (e) {
     console.log(e.target.id);
     mainCharacter.style.left = x - ofsetCharacter + "px";
     mainCharacter.style.top = y - ofsetCharacter + "px";
+
+    switch (e.target.id) {
+        case "door1":
+            mainCharacter.style.backgroundColor = "#FFF00";
+            door1.style.opacity = 0.5;
+            sign.style.opacity = 0.8;
+            break;
+
+        case "sign":
+            sign.style.opacity = 0.5;
+            door1.style.opacity = 0.8;
+            break;
+        default:
+            //explode
+            mainCharacter.style.backgroundColor = "#ff9100";
+            door1.style.opacity = 0.8;
+            sign.style.opacity = 0.8;
+            break;
+    }
+
 }
