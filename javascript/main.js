d500f6cae9b515f1de8da4f668b2f3c0ecd25cef
luuk600 <119942867+luuk600@users.noreply.github.com>
2024-02-08 12:02:18 +0100

    making main character walk

--- javascript/main.js
+++ javascript/main.js
@@ -4,8 +4,14 @@
 const gameWindow = document.getElementById("gameWindow");
 // character reference
 const mainCharacter = document.getElementById("mainCharacter")
-
+const ofsetCharacter = 16;
 
 gameWindow.onclick = function (e) {
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+
     console.log(e.target.id);
+    mainCharacter.style.left = x - ofsetCharacter + "px";
+    mainCharacter.style.top = y - ofsetCharacter + "px";
 }
